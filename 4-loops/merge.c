3d319d69bff642238179d13197a9a50fa01d109c
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2023-10-19 17:14:15 +0800

    +4-loop code template

---
+++ 4-loops/merge.c
@@ -0,0 +1,17 @@
+//
+// Created by hfwei on 2023/10/19.
+//
+
+#include <stdio.h>
+
+#define LEN_L 5
+#define LEN_R 6
+
+int L[LEN_L] = { 1, 3, 5, 7, 9 };
+int R[LEN_R] = { 0, 2, 4, 6, 8, 10 };
+
+int main(void) {
+  // TODO: merge L and R into a sorted array
+
+  return 0;
+}
