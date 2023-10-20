a17debe5a977b58177da164d2e47d8e525066ec0
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2023-10-20 19:41:31 +0800

    +4-loops code

--- 4-loops/game-of-life.c
+++ 4-loops/game-of-life.c
@@ -2,6 +2,10 @@
 // Created by hfwei on 2023/10/19.
 //
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #define SIZE 6
 
 const int board[SIZE][SIZE] = {
@@ -22,6 +26,74 @@ const int board[SIZE][SIZE] = {
 
 int main() {
   // TODO: play game-of-life
+  int old_board[SIZE + 2][SIZE + 2] = { 0 };
+
+  for (int row = 1; row <= SIZE; row++) {
+    for (int col = 1; col <= SIZE; col++) {
+      old_board[row][col] = board[row - 1][col - 1];
+    }
+  }
+
+  // print the initial board
+  for (int row = 1; row <= SIZE; row++) {
+    for (int col = 1; col <= SIZE; col++) {
+      printf("%c ", old_board[row][col] ? '*' : ' ');
+    }
+    printf("\n");
+  }
+  system("clear");
+
+  // old_board = apply the rule > new_board
+  int new_board[SIZE + 2][SIZE + 2] = { 0 };
+
+  for (int i = 0; i < 10; ++i) {
+    for (int row = 1; row <= SIZE; row++) {
+      for (int col = 1; col <= SIZE; col++) {
+        // counting live cells in the neighbour on old_board[row][col]
+        int neighbors =
+            old_board[row - 1][col - 1] +
+                old_board[row - 1][col] +
+                old_board[row - 1][col + 1] +
+                old_board[row][col - 1] +
+                old_board[row][col + 1] +
+                old_board[row + 1][col - 1] +
+                old_board[row + 1][col] +
+                old_board[row + 1][col + 1];
+
+        // apply the rule
+        if (old_board[row][col]) {
+          new_board[row][col] = (neighbors == 2 || neighbors == 3);
+        } else {
+          new_board[row][col] = (neighbors == 3);
+        }
+      }
+    }
+
+    for (int row = 1; row <= SIZE; row++) {
+      for (int col = 1; col <= SIZE; col++) {
+        printf("%c ", new_board[row][col] ? '*' : ' ');
+      }
+      printf("\n");
+    }
+    // Linux: unistd.h
+    sleep(1);
+    // Windows: windows.h
+    // Sleep(1000);
+
+    // Linux: stdlib.h
+    system("clear");
+    // Window: stdlib.h
+    // system("cls");
+
+    // old_board <- new_board
+    for (int row = 1; row <= SIZE; row++) {
+      for (int col = 1; col <= SIZE; col++) {
+        old_board[row][col] = new_board[row][col];
+      }
+    }
+  }
+
+
 
   return 0;
 }
--- 4-loops/merge.c
+++ 4-loops/merge.c
@@ -12,6 +12,29 @@ int R[LEN_R] = { 0, 2, 4, 6, 8, 10 };
 
 int main(void) {
   // TODO: merge L and R into a sorted array
+  int l = 0;
+  int r = 0;
+
+  while (l < LEN_L && r < LEN_R) {
+    if (L[l] <= R[r]) {
+      printf("%d ", L[l]);
+      l++;
+    } else {
+      printf("%d ", R[r]);
+      r++;
+    }
+  }
+
+  // l >= LEN_L || r >= LEN_R
+  while (r < LEN_R) {
+    printf("%d ", R[r]);
+    r++;
+  }
+
+  while (l < LEN_L) {
+    printf("%d ", L[l]);
+    l++;
+  }
 
   return 0;
 }
